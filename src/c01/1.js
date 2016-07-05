9c881484281c22af3358c89467d17e59f256a8ec
mathwiz <mathwiz@gmail.com>
2016-07-05 16:55:57 -0500

    formatting

--- src/c01/1.js
+++ src/c01/1.js
@@ -1,20 +1,20 @@
 function p(exp) {
-  print(exp);
+    print(exp);
 }
 
 p(10);
 
 p(5 + 3 + 4);
 
-p(9-1);
+p(9 - 1);
 
-p(6/2);
+p(6 / 2);
 
 p((2 * 4) + (4 - 6));
 
 var a = 3;
 
-var b =  a + 1;
+var b = a + 1;
 
 p(a + b + a * b);
 
@@ -22,8 +22,12 @@ p(a == b);
 
 p((b > a) && (b < (a * b)) ? b : a);
 
-p( (function() {if (a == 4) return 6; else if (b == 4) return 6 + 7 + a; else return 25; })() );
+p((function () {
+    if (a == 4) return 6; else if (b == 4) return 6 + 7 + a; else return 25;
+})());
 
 p((b > a ? b : a) + 2);
 
-p( (function() {if (a > b) return a; else if (a < b) return b; else return -1; })() * (a + 1) );
+p((function () {
+        if (a > b) return a; else if (a < b) return b; else return -1;
+    })() * (a + 1));
