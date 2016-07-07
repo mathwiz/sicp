2307ce37698b775e0c32c3e18eea21f5387b0c7e
mathwiz <mathwiz@gmail.com>
2016-07-06 19:30:50 -0500

    List version

--- src/c01/1.js
+++ src/c01/1.js
@@ -1,33 +1,33 @@
-function p(exp) {
-    print(exp);
-}
+z = [];
 
-p(10);
+z.push(10);
 
-p(5 + 3 + 4);
+z.push(5 + 3 + 4);
 
-p(9 - 1);
+z.push(9 - 1);
 
-p(6 / 2);
+z.push(6 / 2);
 
-p((2 * 4) + (4 - 6));
+z.push((2 * 4) + (4 - 6));
 
 var a = 3;
 
 var b = a + 1;
 
-p(a + b + a * b);
+z.push(a + b + a * b);
 
-p(a == b);
+z.push(a == b);
 
-p((b > a) && (b < (a * b)) ? b : a);
+z.push((b > a) && (b < (a * b)) ? b : a);
 
-p((function () {
+z.push((function () {
     if (a == 4) return 6; else if (b == 4) return 6 + 7 + a; else return 25;
 })());
 
-p((b > a ? b : a) + 2);
+z.push((b > a ? b : a) + 2);
 
-p((function () {
+z.push((function () {
         if (a > b) return a; else if (a < b) return b; else return -1;
     })() * (a + 1));
+
+z.forEach(function (it) { print(it) });
