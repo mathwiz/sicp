e5109ee776c561830a202929662f34425466f04b
mathwiz <mathwiz@gmail.com>
2018-02-26 16:11:58 -0500

    scala #1

--- src/c01/1.scala
+++ src/c01/1.scala
@@ -1,6 +1,18 @@
 /**
   * Created by Yohan on 7/5/2016.
   */
-def p(x:Int) = println(x)
 
-p(10)
+10;
+5 + 3 + 4;
+9 - 1;
+6 / 2;
+(2 * 4) + (4 - 6);
+val a = 3;
+val b = a + 1;
+a + b + (a * b);
+a == b;
+if ((b > a) && (b < a*b)) b else a;
+if (a==4) 6 else if (b==4) 6+7+a else 25;
+2 + (if (b > a) b else a);
+(if (a > b) a else if (a < b) b else -1) * (a + 1);
+
