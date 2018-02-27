a2d30afbed9c0b79f404c358c49f4cc4284175ad
mathwiz <mathwiz@gmail.com>
2018-02-27 09:38:26 -0500

    languages: js, py, rb, rkt?

--- src/c01/1.rb
+++ src/c01/1.rb
@@ -1,4 +1,30 @@
-a = Array.new
-a.push(10)
-a.push(5+3+4)
-a.each { |i| puts i }
+z = Array.new
+
+z.push(10)
+
+z.push(5+3+4)
+
+z.push(9-1)
+
+z.push(6/2)
+
+z.push((2*4) + (4-6))
+
+a = 3
+
+b = a + 1
+
+z.push(a+b+a*b)
+
+z.push(a==b)
+
+z.push(b>a && (b<(a*b)) ? b : a)
+
+z.push(a == 4 ? 6 : b == 4 ? 6 + 7 + a : 25)
+
+z.push((b > a ? b : a) + 2)
+
+z.push((a > b ? a : a < b ? b : -1) * (a + 1))
+ 
+
+z.each { |i| puts i }
