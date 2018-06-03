84f40f530a2863b9e0cd33c41a0ac9fb4137baf0
mathwiz <mathwiz@gmail.com>
2018-06-03 15:21:31 -0400

    clojure?

--- src/c01/1.clj
+++ src/c01/1.clj
@@ -1,48 +1,42 @@
-;; hack to show the evaluations in this file as separate lines in console
-(def p println)
+10
 
-(p 10)
+(+ 5 3 4)
 
-(p (+ 5 3 4))
+(- 9 1)
 
-(p (- 9 1))
+(/ 6 2)
 
-(p (/ 6 2))
-
-(p (+ (* 2 4) 
-      (- 4 6)))
+(+ (* 2 4) 
+   (- 4 6))
 
 (def a 3)
 
 (def b (+ a 1))
 
-(p (+ a b (* a b)))
+(+ a b (* a b))
 
-(p (= a b))
+(= a b)()
 
-(p 
- (if (and (> b a) 
-          (< b (* a b))) 
-   b
-   a))
+(if (and (> b a) 
+         (< b (* a b))) 
+  b
+  a)
 
-(p 
- (cond (= a 4) 
-       6
-       (= b 4) 
-       (+ 6 7 a) 
-       :else 25))
+(cond (= a 4) 
+      6
+      (= b 4) 
+      (+ 6 7 a) 
+      :else 25)
 
-(p (+ 2 
-      (if (> b a) 
-        b
-        a)))
+(+ 2 
+   (if (> b a) 
+     b
+     a))
 
-(p
- (*
-  (cond (> a b) 
-        a
-        (< a b)
-        b 
-        :else -1)
-  (+ a 1)))
+(*
+ (cond (> a b) 
+       a
+       (< a b)
+       b 
+       :else -1)
+ (+ a 1))
