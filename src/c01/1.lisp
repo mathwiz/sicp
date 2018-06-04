f16a663771246be56b63df41edd4bd1d6bad030c
mathwiz <yohanlee@yahoo.com>
2018-06-03 22:59:19 -0400

    on mac

--- src/c01/1.lisp
+++ src/c01/1.lisp
@@ -16,9 +16,15 @@
 
 (= a b)
 
-(if (and (> b a) (< b (* a b)))
-    b
-    a)
+(cond ((and (> b a) (< b (* a b))) b)
+      (else a))
 
+(cond ((= a 4) 6)
+      ((= b 4) (+ 6 7 a))
+      (else 25))
 
+(+ 2 (cond ((> b a) b) (else a)))
 
+(* (cond ((> a b) a) ((< a b) b) (else -1))
+   (+ a 1))
+	
