13354552db23019382ed54a76518aa2f1efeb533
mathwiz <mathwiz@gmail.com>
2018-06-05 07:55:13 -0400

    just use ruby expressions

--- src/c01/1.rb
+++ src/c01/1.rb
@@ -1,30 +1,26 @@
-z = Array.new
+10
 
-z.push(10)
+5+3+4
 
-z.push(5+3+4)
+9-1
 
-z.push(9-1)
+6/2
 
-z.push(6/2)
-
-z.push((2*4) + (4-6))
+(2*4) + (4-6)
 
 a = 3
 
 b = a + 1
 
-z.push(a+b+a*b)
+a+b+a*b
 
-z.push(a==b)
+a==b
 
-z.push(b>a && (b<(a*b)) ? b : a)
+b>a && (b<(a*b)) ? b : a
 
-z.push(a == 4 ? 6 : b == 4 ? 6 + 7 + a : 25)
+a == 4 ? 6 : b == 4 ? 6 + 7 + a : 25
 
-z.push((b > a ? b : a) + 2)
+(b > a ? b : a) + 2
 
-z.push((a > b ? a : a < b ? b : -1) * (a + 1))
+(a > b ? a : a < b ? b : -1) * (a + 1)
  
-
-z.each { |i| puts i }
