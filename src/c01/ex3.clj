a4f653c43fd01da44eb83f38e13822fb7d3b9e54
mathwiz <mathwiz@gmail.com>
2018-06-17 17:26:00 -0400

    load file from clojure script

--- src/c01/ex3.clj
+++ src/c01/ex3.clj
@@ -1,12 +1,20 @@
-(defn sum-sq [x y] (+ (* x x) (* y y)))
+(load-file "functions.clj")
 
 (defn larger-sq [x y z]
-  (cond (= (min x y z) x) (sum-sq y z)
-        (= (min x y z) y) (sum-sq x z)
-        :else (sum-sq x y)))
+  (int
+    (cond (= (min x y z) x) (sum-squares [y z])
+          (= (min x y z) y) (sum-squares [x z])
+          :else             (sum-squares [x y]))))
 
 
-(require 'clojure.test)
+(require
+  'clojure.test)
+
+(clojure.test/is (= (sum-squares nil) 0.0))
+(clojure.test/is (= (sum-squares [1]) 1.0))
+(clojure.test/is (= (sum-squares [1 2]) 5.0))
+(clojure.test/is (= (sum-squares [1 2 3]) 14.0))
+(clojure.test/is (= (sum-squares [1 2 3 4]) 30.0))
 (clojure.test/is (= (larger-sq 1 2 3) 13))
 (clojure.test/is (= (larger-sq 4 2 3) 25))
 (clojure.test/is (= (larger-sq 4 5 3) 41))
--- src/c01/functions.clj
+++ src/c01/functions.clj
@@ -3,3 +3,6 @@
    (cond (= (eval sexp) expected) (str "passed: " expected)
          :else                    (str "should be " expected))))
 
+
+(defn sum-squares [xs] (reduce #(+ %1 (* (Math/pow %2 2))) 0.0 xs))
+
