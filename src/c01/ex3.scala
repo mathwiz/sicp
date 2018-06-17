da0a7b578c7c17fcf92bd26c205368c0003574ec
mathwiz <mathwiz@gmail.com>
2018-06-17 08:30:37 -0400

    unit tests for scala

--- src/c01/ex3.scala
+++ src/c01/ex3.scala
@@ -11,3 +11,8 @@ def larger_sq (x: Int, y: Int, z: Int): Int = {
 def sum_sq (x: Int, y: Int): Int = {
   x * x + y * y
 }
+
+val shouldEqual_13 = larger_sq(1,2,3) == 13
+val shouldEqual_25 = larger_sq(4,2,3) == 25
+val shouldEqual_41 = larger_sq(4,5,3) == 41
+val shouldEqual_20 = larger_sq(4,2,1) == 20
