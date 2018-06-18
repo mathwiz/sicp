6092ba888b17b73e8027e259b47859af3dcefe3a
mathwiz <mathwiz@gmail.com>
2018-06-18 16:03:21 -0400

    rational numbers

--- src/c01/ex2.pl
+++ src/c01/ex2.pl
@@ -1,6 +1,8 @@
 #!/usr/bin/perl
 use strict;
 use warnings FATAL => 'all';
+use Math::BigRat;
 
-my $exp = (5 + 4 + (2 - (3 - (6 + (4 / 5.0))))) / (3 * (6 - 2) * (2 - 7));
+
+my $exp = Math::BigRat->new (5 + 4 + (2 - (3 - (6 + (4 / 5.0))))) / (3 * (6 - 2) * (2 - 7));
 print "$exp\n";
--- src/c01/ex2.py
+++ src/c01/ex2.py
@@ -1,1 +1,4 @@
-print( (5 + 4 + (2 - (3 - (6 + (4 / 5.0))))) / (3 * (6 - 2) * (2 - 7)) )
+from fractions import Fraction
+
+exp = Fraction( 5 + 4 + (2 - (3 - (6 + Fraction(4 , 5)))) , (3 * (6 - 2) * (2 - 7)) )
+print( exp )
