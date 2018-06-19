7f27c06a294cbc6c89c6dd32b3640413d5c1c2df
mathwiz <mathwiz@gmail.com>
2018-06-19 15:35:42 -0400

    R code

---
+++ src/c01/ex1.R
@@ -0,0 +1,25 @@
+print(10)
+
+print(5 + 3 + 4)
+
+print(9 - 1)
+
+print(6 / 2)
+
+print((2 * 4) + (4 - 6))
+
+a <- 3
+
+b <- a + 1
+
+print(a + b + a * b)
+
+print(a == b)
+
+print(if (b > a && b < a * b)b else a)
+
+print(if (a == 4)6 else if (b == 4)6 + 7 + a else 25)
+
+print(2 + (if (b > a)b else a))
+
+print((if (a > b)a else if (a < b)b else - 1) * (a + 1))
