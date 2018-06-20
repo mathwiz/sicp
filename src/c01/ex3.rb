702d01231d6e4ddb34fb8e5b81928ee4aa342599
mathwiz <mathwiz@gmail.com>
2018-06-19 21:14:44 -0400

    temporary unit test class

--- src/c01/ex3.rb
+++ src/c01/ex3.rb
@@ -1,3 +1,4 @@
+#require 'test/unit'
 require_relative 'functions'
 
 def larger_sq(x, y, z)
@@ -11,9 +12,22 @@ def larger_sq(x, y, z)
   end
 end
 
+class Test1 < TempTestCase #< Test::Unit::TestCase
+  def test_sum_squares
+    assert_equal(14, sum_squares([1,2,3]))
+  end
 
-p sum_squares([1,2,3])
-p larger_sq(2,3,4)
-p larger_sq(5,3,4)
-p larger_sq(1,1,1)
+  def test_larger_sq
+    assert_equal(25, larger_sq(2,3,4))
+    assert_equal(41, larger_sq(5,3,4))
+    assert_equal(2, larger_sq(1,1,1))
+  end
 
+  def test_string_eq
+    assert_equal("foo", "foo?")
+  end
+end
+
+Test1.new("Test").test_sum_squares
+Test1.new("Test").test_larger_sq
+#Test1.new("Won't Pass").test_string_eq
--- src/c01/functions.rb
+++ src/c01/functions.rb
@@ -1,3 +1,14 @@
+# Until we are able to load test/unit
+class TempTestCase
+  def initialize(name)
+  end
+
+  def assert_equal(expected, actual)
+    p "failed #{expected}" unless expected == actual
+  end
+end
+
+
 def sum_squares(xs)
   xs.reduce(0) {|a,x| a + x*x }
 end
