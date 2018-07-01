64a5dc08a1f0d6c4ebdf3d5390620a28d6ea1a1e
mathwiz <mathwiz@gmail.com>
2018-07-01 15:13:50 -0400

    javascript lambda

--- src/c01/ex4.js
+++ src/c01/ex4.js
@@ -1,5 +1,10 @@
 load('functions.js');
 
 function a_plus_abs_b(a, b) {
-
+    return ( b > 0 ? function() { return a + b } : function() { return a - b } ).call();
 }
+
+check_expect("a_plus_abs_b(1,2)", 3);
+check_expect("a_plus_abs_b(1,-2)", 3);
+check_expect("a_plus_abs_b(-1,2)", 1);
+check_expect("a_plus_abs_b(-1,-2)", 1);
