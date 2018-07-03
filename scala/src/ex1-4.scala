b9f919232d51284cf58f51ed32feace394e289cb
mathwiz <mathwiz@gmail.com>
2018-07-03 17:03:32 -0400

    why can't I use + as a function

--- scala/src/ex1-4.scala
+++ scala/src/ex1-4.scala
@@ -1,7 +1,14 @@
-def a_plus_abs_b (a: Int, b: Int): Int = {
+def a_plus_abs_b_lambda (a: Int, b: Int): Int = {
   (if (b > 0) (() => a + b) else (() => a - b)) ()
 }
 
+def a_plus_abs_b_lambda2 (a: Int, b: Int): Int = {
+  (if (b > 0) (x: Int) => a.+(x) else (x:Int) => a.-(x)) (b)
+}
+
+def a_plus_abs_b (a: Int, b: Int): Int = {
+  a_plus_abs_b_lambda2 (a, b)
+}
 
 val shouldBeAdded = a_plus_abs_b(1, 2) == 3
 val shouldBeSubtracted = a_plus_abs_b(1, -2) == 3
