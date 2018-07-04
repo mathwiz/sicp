f03ba4be3313a8a149a17eeea849970d167b3a47
mathwiz <mathwiz@gmail.com>
2018-07-04 13:29:58 -0400

    scala assert

--- scala/src/ex1-3.scala
+++ scala/src/ex1-3.scala
@@ -10,7 +10,8 @@ def larger_sq (x: Int, y: Int, z: Int): Int = {
   }
 }
 
-val shouldEqual_13 = larger_sq(1,2,3) == 13
-val shouldEqual_25 = larger_sq(4,2,3) == 25
-val shouldEqual_41 = larger_sq(4,5,3) == 41
-val shouldEqual_20 = larger_sq(4,2,1) == 20
+assert (larger_sq(1,2,3) == 13)
+assert (larger_sq(4,2,3) == 25)
+assert (larger_sq(4,5,3) == 41)
+assert (larger_sq(4,2,1) == 20)
+println("done")
--- scala/src/ex1-4.scala
+++ scala/src/ex1-4.scala
@@ -20,7 +20,8 @@ def a_plus_abs_b (a: Int, b: Int): Int = {
   a_plus_abs_b_operator2 (a, b)
 }
 
-val shouldBeAdded = a_plus_abs_b(1, 2) == 3
-val shouldBeSubtracted = a_plus_abs_b(1, -2) == 3
-val shouldBeAddedWithNegA = a_plus_abs_b(-1, 2) == 1
-val shouldBeSubtractedWithNegA = a_plus_abs_b(-1, -2) == 1
+assert (a_plus_abs_b(1, 2) == 3)
+assert (a_plus_abs_b(1, -2) == 3)
+assert (a_plus_abs_b(-1, 2) == 1)
+assert (a_plus_abs_b(-1, -2) == 1)
+println("done")
