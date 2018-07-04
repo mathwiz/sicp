4dd62ab5b411e4fcd741056174545eedc180e068
mathwiz <mathwiz@gmail.com>
2018-07-03 22:17:51 -0400

    function literal

--- scala/src/ex1-4.scala
+++ scala/src/ex1-4.scala
@@ -6,8 +6,18 @@ def a_plus_abs_b_lambda2 (a: Int, b: Int): Int = {
   (if (b > 0) (x: Int) => a.+(x) else (x:Int) => a.-(x)) (b)
 }
 
+def a_plus_abs_b_operator (a: Int, b: Int): Int = {
+  List(b).foldLeft(a) (if (b > 0) _+_ else _-_)
+}
+
+def a_plus_abs_b_operator2 (a: Int, b: Int): Int = {
+  val my_add : (Int, Int) => Int = _+_
+  val my_sub : (Int, Int) => Int = _-_
+  (if (b > 0) my_add else my_sub) (a, b)
+}
+
 def a_plus_abs_b (a: Int, b: Int): Int = {
-  a_plus_abs_b_lambda2 (a, b)
+  a_plus_abs_b_operator2 (a, b)
 }
 
 val shouldBeAdded = a_plus_abs_b(1, 2) == 3
