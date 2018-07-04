b07b070dcc80a0245c44ca84837c6c75f4e90f1b
mathwiz <mathwiz@gmail.com>
2018-07-04 16:20:09 -0400

    racket new-if

---
+++ racket/ex1-6.rkt
@@ -0,0 +1,28 @@
+(define (sqrt-iter guess x) 
+  (if (good-enough? guess x) guess (sqrt-iter (improve guess x) x)))
+
+(define (improve guess x) 
+  (average guess (/ x guess)))
+
+(define (average x y) 
+  (/ (+ x y) 2))
+
+(define (good-enough?  guess x)
+  (< (abs (- (square guess) x)) 0.001))
+
+(define (my-sqrt x)
+  (sqrt-iter 1.0 x))
+
+(define (square x)
+  (* x x))
+
+(define (new-if predicate then-clause else-clause)
+  (cond (predicate then-clause)
+        (else else-clause)))
+
+(define (bad-sqrt-iter guess x)
+  (new-if (good-enough? guess x) guess (bad-sqrt-iter (improve guess x) x)))
+
+;; this will not complete
+(define (my-bad-sqrt x)
+  (bad-sqrt-iter 1.0 x))
