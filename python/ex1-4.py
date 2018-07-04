b45798467e70a3d6878695d4bbd8aa12bd5ca002
mathwiz <mathwiz@gmail.com>
2018-07-03 22:43:39 -0400

    remove lint warning

--- python/ex1-4.py
+++ python/ex1-4.py
@@ -1,20 +1,21 @@
 import nose
 import operator
 
+
 def a_plus_abs_b_old(a, b):
-    return ((lambda: a + b) if b > 0 else (lambda: a - b)) ()
+    return ((lambda: a + b) if b > 0 else (lambda: a - b))()
+
 
 def a_plus_abs_b(a, b):
-    return (operator.add if b > 0 else operator.sub) (a,b)
+    return (operator.add if b > 0 else operator.sub)(a, b)
 
 
 def test_1():
-    assert a_plus_abs_b(1,2) == 3
-    assert a_plus_abs_b(1,-2) == 3
-    assert a_plus_abs_b(-1,2) == 1
-    assert a_plus_abs_b(-1,-2) == 1
+    assert a_plus_abs_b(1, 2) == 3
+    assert a_plus_abs_b(1, -2) == 3
+    assert a_plus_abs_b(-1, 2) == 1
+    assert a_plus_abs_b(-1, -2) == 1
 
 
 if __name__ == '__main__':
     nose.runmodule()
-
