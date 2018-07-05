8c1705bc16f6b139cf847b5b3f4a9a83d5a9223d
mathwiz <mathwiz@gmail.com>
2018-07-05 19:59:18 -0400

    racket

--- racket/ex1-6.rkt
+++ racket/ex1-6.rkt
@@ -1,3 +1,8 @@
+#lang racket
+(require rackunit)
+
+(provide my-sqrt my-bad-sqrt)
+
 (define (sqrt-iter guess x) 
   (if (good-enough? guess x) guess (sqrt-iter (improve guess x) x)))
 
@@ -26,3 +31,8 @@
 ;; this will not complete
 (define (my-bad-sqrt x)
   (bad-sqrt-iter 1.0 x))
+
+
+(define delta 0.00001)
+(check-= (my-sqrt 2) 1.4142135623730951  delta)
+(write "done")
