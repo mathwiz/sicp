65a81145833900ec322404a5512e508bba48c395
mathwiz <mathwiz@gmail.com>
2018-07-04 20:26:25 -0400

    more paradigmatic functional java

--- java/src/ex1_4.java
+++ java/src/ex1_4.java
@@ -2,23 +2,23 @@ import java.util.function.Supplier;
 
 public class ex1_4 {
 
-    static class a_plus_abs_b {
+    static class a_plus_abs_b implements Supplier<Integer> {
         Supplier<Integer> op;
 
         a_plus_abs_b(int a, int b) {
             op = b > 0 ? () -> a + b : () -> a - b;
         }
 
-        int intValue() {
+        public Integer get() {
             return op.get();
         }
     }
 
     public static void main(String[] args) {
-        System.out.println(new a_plus_abs_b(1,2).intValue());
-        System.out.println(new a_plus_abs_b(1,-2).intValue());
-        System.out.println(new a_plus_abs_b(-1,2).intValue());
-        System.out.println(new a_plus_abs_b(-1,-2).intValue());
+        System.out.println(new a_plus_abs_b(1,2).get());
+        System.out.println(new a_plus_abs_b(1,-2).get());
+        System.out.println(new a_plus_abs_b(-1,2).get());
+        System.out.println(new a_plus_abs_b(-1,-2).get());
     }
 }
 
---
+++ scala/src/ex1-6.scala
@@ -0,0 +1,2 @@
+def square (x: Int) : Int = x * x
+
