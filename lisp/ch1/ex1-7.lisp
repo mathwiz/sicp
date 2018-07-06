5b2136ed10dcab85c241199b8ac7853f65999396
mathwiz <mathwiz@gmail.com>
2018-07-06 13:55:26 -0400

    lisp corrected

--- lisp/ch1/ex1-7.lisp
+++ lisp/ch1/ex1-7.lisp
@@ -1,18 +1,23 @@
-(defn sqrt-iter (guess x) 
+(defun sqrt-iter (guess x) 
   (if (good-enough? guess x) guess (sqrt-iter (improve guess x) x)))
 
-(defn improve (guess x) 
+(defun improve (guess x) 
   (average guess (/ x guess)))
 
-(defn average (x y) 
+(defun average (x y) 
   (/ (+ x y) 2))
 
-(defn good-enough?  (guess x)
+(defun good-enough?  (guess x)
   (< (abs (- (square guess) x)) (/ guess 100000)))
 
-(defn my-sqrt (x)
+(defun my-sqrt (x)
   (sqrt-iter 1.0 x))
 
-(defn square (x)
+(defun square (x)
   (* x x))
 
+
+(print (my-sqrt 0.02))
+(print (sqrt 0.02))
+(print (my-sqrt 0.0002))
+(print (sqrt 0.0002))
