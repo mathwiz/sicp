ed17298ef08118cb0b3dce975ff1fd797c8e3ddc
mathwiz <mathwiz@gmail.com>
2018-07-05 20:21:41 -0400

    lisp

--- lisp/ch1/ex1-7.lisp
+++ lisp/ch1/ex1-7.lisp
@@ -1,31 +1,18 @@
-#lang racket
-(require rackunit)
-
-; load with (require "ex1-7.rkt")
-(provide my-sqrt)
-
-(define (sqrt-iter guess x) 
+(defun sqrt-iter (guess x) 
   (if (good-enough? guess x) guess (sqrt-iter (improve guess x) x)))
 
-(define (improve guess x) 
+(defun improve (guess x) 
   (average guess (/ x guess)))
 
-(define (average x y) 
+(defun average (x y) 
   (/ (+ x y) 2))
 
-(define (good-enough?  guess x)
+(defun good-enough?  (guess x)
   (< (abs (- (square guess) x)) (/ guess 100000)))
 
-(define (my-sqrt x)
+(defun my-sqrt (x)
   (sqrt-iter 1.0 x))
 
-(define (square x)
+(defun square (x)
   (* x x))
 
-
-(define delta 0.00001)
-(define small (expt 2 -10))
-(check-= (my-sqrt 2) (sqrt 2)  delta)
-(check-= (my-sqrt .2) (sqrt .2) delta)
-(check-= (my-sqrt small) (sqrt small) delta)
-(write "done")
