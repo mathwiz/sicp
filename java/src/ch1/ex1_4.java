08c69811a7ed5f16e0b461897bd12c7c70fe9293
mathwiz <mathwiz@gmail.com>
2018-07-07 19:59:25 -0400

    function literals in java

--- java/src/ch1/ex1_4.java
+++ java/src/ch1/ex1_4.java
@@ -1,9 +1,17 @@
 package ch1;
 
 import java.util.Arrays;
+import java.util.function.BiFunction;
 import java.util.function.Supplier;
 
 public class ex1_4 {
+    static int add(int x, int y) {
+        return x + y;
+    }
+
+    static int sub(int x, int y) {
+        return x - y;
+    }
 
     static class a_plus_abs_b implements Supplier<Integer> {
         Supplier<Integer> op;
@@ -18,38 +26,24 @@ public class ex1_4 {
     }
 
 
-    static class a_plus_abs_b_function_literal implements Supplier<Integer> {
-        int a;
-        int b;
-
-        a_plus_abs_b_function_literal(int a, int b) {
-            this.a = a;
-            this.b = b;
-        }
-
-        static int add(int x, int y) {
-            return x + y;
-        }
-
-        static int sub(int x, int y) {
-            return x - y;
-        }
-
-        public Integer get() {
+    static class a_plus_abs_b_function_literal implements BiFunction<Integer, Integer, Integer> {
+        public Integer apply(Integer a, Integer b) {
             return Arrays.asList(b).stream()
-                    .reduce(a, b > 0 ? a_plus_abs_b_function_literal::add : a_plus_abs_b_function_literal::sub);
+                    .reduce(a, b > 0 ? ex1_4::add : ex1_4::sub);
         }
     }
 
     public static void main(String[] args) {
+        System.out.println("Anonymous lambda");
         System.out.println(new a_plus_abs_b(1, 2).get());
         System.out.println(new a_plus_abs_b(1, -2).get());
         System.out.println(new a_plus_abs_b(-1, 2).get());
         System.out.println(new a_plus_abs_b(-1, -2).get());
-        System.out.println(new a_plus_abs_b_function_literal(1, 2).get());
-        System.out.println(new a_plus_abs_b_function_literal(1, -2).get());
-        System.out.println(new a_plus_abs_b_function_literal(-1, 2).get());
-        System.out.println(new a_plus_abs_b_function_literal(-1, -2).get());
+        System.out.println("Function literal");
+        System.out.println(new a_plus_abs_b_function_literal().apply(1, 2));
+        System.out.println(new a_plus_abs_b_function_literal().apply(1, -2));
+        System.out.println(new a_plus_abs_b_function_literal().apply(-1, 2));
+        System.out.println(new a_plus_abs_b_function_literal().apply(-1, -2));
     }
 }
 
