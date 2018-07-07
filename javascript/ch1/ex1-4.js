56f5233d001dc74b93a80bb220b65379f77297e6
mathwiz <mathwiz@gmail.com>
2018-07-07 19:32:01 -0400

    refactor js function ref

--- javascript/ch1/ex1-4.js
+++ javascript/ch1/ex1-4.js
@@ -4,12 +4,15 @@ function a_plus_abs_b_anon(a, b) {
     return ( b > 0 ? function() { return a + b } : function() { return a - b } ).call();
 }
 
-function a_plus_abs_b(a, b) {
-    return ( b > 0 ? function() { return a + b } : function() { return a - b } ).call();
+
+function a_plus_abs_b_named(a, b) {
+    var add_fn = function(x, y) { return x + y };
+    var sub_fn = function(x, y) { return x - y };
+    return ( b > 0 ? add_fn : sub_fn ).call(this, a, b);
 }
 
 function a_plus_abs_b(a, b) {
-    return a_plus_abs_b_anon(a, b);
+    return a_plus_abs_b_named(a, b);
 }
 
 check_expect("a_plus_abs_b(1,2)", 3);
