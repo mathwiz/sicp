cf71df2e2aa786fbcadc4cce6e17a6ac6ba8443a
mathwiz <mathwiz@gmail.com>
2018-07-07 19:51:50 -0400

    function literals in java

--- java/src/ch1/ex1_4.java
+++ java/src/ch1/ex1_4.java
@@ -1,5 +1,6 @@
 package ch1;
 
+import java.util.Arrays;
 import java.util.function.Supplier;
 
 public class ex1_4 {
@@ -16,11 +17,39 @@ public class ex1_4 {
         }
     }
 
+
+    static class a_plus_abs_b_function_literal implements Supplier<Integer> {
+        int a;
+        int b;
+
+        a_plus_abs_b_function_literal(int a, int b) {
+            this.a = a;
+            this.b = b;
+        }
+
+        static int add(int x, int y) {
+            return x + y;
+        }
+
+        static int sub(int x, int y) {
+            return x - y;
+        }
+
+        public Integer get() {
+            return Arrays.asList(b).stream()
+                    .reduce(a, b > 0 ? a_plus_abs_b_function_literal::add : a_plus_abs_b_function_literal::sub);
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println(new a_plus_abs_b(1,2).get());
-        System.out.println(new a_plus_abs_b(1,-2).get());
-        System.out.println(new a_plus_abs_b(-1,2).get());
-        System.out.println(new a_plus_abs_b(-1,-2).get());
+        System.out.println(new a_plus_abs_b(1, 2).get());
+        System.out.println(new a_plus_abs_b(1, -2).get());
+        System.out.println(new a_plus_abs_b(-1, 2).get());
+        System.out.println(new a_plus_abs_b(-1, -2).get());
+        System.out.println(new a_plus_abs_b_function_literal(1, 2).get());
+        System.out.println(new a_plus_abs_b_function_literal(1, -2).get());
+        System.out.println(new a_plus_abs_b_function_literal(-1, 2).get());
+        System.out.println(new a_plus_abs_b_function_literal(-1, -2).get());
     }
 }
 
