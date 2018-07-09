6cb2cc04969f73f1f4ad029d6c37f3e8a383d8a2
mathwiz <mathwiz@gmail.com>
2018-07-09 17:25:31 -0400

    more R-ish

--- rlang/ch1/ex1-3.R
+++ rlang/ch1/ex1-3.R
@@ -15,10 +15,23 @@ largerSq <- function(x, y, z) {
     Reduce(x=square(args[args!=min(args)]), f=sum)
 }
 
+largerSq2 <- function(x, y, z) {
+    args <- c(x, y, z)
+    elems <- args[args!=min(args)]
+    xs <- if (length(elems) == 0) args[-1] else elems # use any 2 elements if all args are equal
+    sum(xs*xs)
+}
+
 
 # Run in consolec
 # > library(testthat)
 # > test_file('R/sicp1-3.R')
+
+test_that("largerSq2 returns correct number", {
+    expect_equal(largerSq2(2, 3, 4), 25)
+    expect_equal(largerSq2(5, 3, 4), 41)
+    expect_equal(largerSq2(1, 1, 1), 2)
+})
 
 test_that("largerSq returns correct number", {
     expect_equal(largerSq(2, 3, 4), 25)
