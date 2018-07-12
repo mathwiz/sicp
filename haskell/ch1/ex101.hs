326b5bad0d8ff701a8a29b88386facd390b147e6
mathwiz <mathwiz@gmail.com>
2018-07-12 16:20:50 -0400

    haskell

--- haskell/ch1/ex101.hs
+++ haskell/ch1/ex101.hs
@@ -1,29 +1,27 @@
-main = putStrLn "Exercise 1 Chapter 1"
+-- Run with ghc -e ":script ex101.hs"
 
-{-
-let e1 = 10
+10
 
-let e2 = 5 + 3 + 4
+5 + 3 + 4
 
-let e3 = 9 - 1
+9 - 1
 
-let e4 = 6 / 2
+6 / 2
 
-let e5 = (2 * 4) + (4 - 6)
+(2 * 4) + (4 - 6)
 
 let a = 3
 
 let b = a + 1
 
-let e6 = a + b + (a * b)
+a + b + (a * b)
 
-let e7 = a == b
+a == b
 
-let e8 = if ((b > a) && (b < a*b)) b else a
+if ((b > a) && (b < a*b)) then b else a
 
-let e9 = if (a==4) then 6 else if (b==4) then 6+7+a else 25
+if (a==4) then 6 else if (b==4) then 6+7+a else 25
 
-let e10 = 2 + (if (b > a) then b else a)
+2 + (if (b > a) then b else a)
 
-let e11 = (if (a > b) then a else if (a < b) then b else -1) * (a + 1)
--}
+(if (a > b) then a else if (a < b) then b else -1) * (a + 1)
