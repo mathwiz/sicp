24306a8f0066bfc09b968e549927665020f7a1f2
mathwiz <mathwiz@gmail.com>
2018-07-12 16:30:33 -0400

    haskell file examples

--- haskell/ch1/Test.hs
+++ haskell/ch1/Test.hs
@@ -1,2 +1,9 @@
-module Test where
+-- Load from ghci with :load Test
+-- Test by running > fac 6
+-- or
+-- Run from shell $ runghc Test.hs
 
+main = print (fac 20)
+
+fac 0 = 1
+fac n = n * fac (n - 1)
