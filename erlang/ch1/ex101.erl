e5549729b7a5b848ba09b4140ccd53634323dfbc
mathwiz <mathwiz@gmail.com>
2018-07-25 09:36:39 -0400

    erlang expressions

--- erlang/ch1/ex101.erl
+++ erlang/ch1/ex101.erl
@@ -1,3 +1,6 @@
+%%% load with > c("ex101.erl").
+%%% run with > ex101:expressions().
+
 -module(ex101).
 -author("Yohan").
 
@@ -6,5 +9,15 @@
 
 expressions() ->
   io:fwrite("~w\n",[10]),
-  io:fwrite("~w\n",[5+3+4]),
-  io:fwrite("~w\n",[-1]).
+  io:fwrite("~w\n",[5 + 3 + 4]),
+  io:fwrite("~w\n",[9 - 1]),
+  io:fwrite("~w\n",[6 / 2]),
+  io:fwrite("~w\n",[(2 * 4) + (4 - 6)]),
+  A = 3,
+  B = A + 1,
+  io:fwrite("~w\n",[A + B + (A * B)]),
+  io:fwrite("~w\n",[A == B]),
+  io:fwrite("~w\n",[if (B > A) and (B < (A * B)) -> B; true -> A end]),
+  io:fwrite("~w\n",[if (A == 4) -> 6; (B == 4) -> 6 + 7 + A; true -> 25 end]),
+  io:fwrite("~w\n",[2 + (if (B > A) -> B; true -> A end)]),
+  io:fwrite("~w\n",[(if (A > B) -> A; (A < B) -> B; true -> -1 end) * (A + 1)]).
