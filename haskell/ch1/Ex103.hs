3ab190187a5412716d5810800ee166d7a6cd8d37
mathwiz <mathwiz@gmail.com>
2018-07-26 09:25:56 -0400

    bug fix

--- haskell/ch1/Ex103.hs
+++ haskell/ch1/Ex103.hs
@@ -1,10 +1,11 @@
 -- :load "Ex103.hs"
+-- run with > larger_sq 1 2 3
 
 module Ex103 where
 
 larger_sq x y z =
-    if x < y && x < z
+    if x <= y && x <= z
     then y * y + z * z
-    else if y < x && y < z
+    else if y <= x && y <= z
     then x * x + z * z
     else x * x + y * y
