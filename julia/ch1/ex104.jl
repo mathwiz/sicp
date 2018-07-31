10f6b3c48db966e392540c7bf8dad1c37f5f2b05
mathwiz <mathwiz@gmail.com>
2018-07-31 09:31:34 -0400

    julia operator as function

--- julia/ch1/ex104.jl
+++ julia/ch1/ex104.jl
@@ -9,4 +9,12 @@ function a_plus_abs_b_lambda(a, b)
    ( b > 0 ? (x->a+x) : (x->a-x) )(b)
 end
 
-a_plus_abs_b(a, b) = a_plus_abs_b_lambda(a, b)
+function a_plus_abs_b_operator(a, b)
+   (if b > 0
+       +
+   else
+       -
+   end)(a,b)
+end
+
+a_plus_abs_b(a, b) = a_plus_abs_b_operator(a, b)
