2c7b643c5b2e12fd3b5ee2cb84da17573d29251e
mathwiz <mathwiz@gmail.com>
2018-08-02 15:22:33 -0400

    iterative and recursive processes

--- java/src/ch1/ex109.java
+++ java/src/ch1/ex109.java
@@ -2,6 +2,15 @@ package ch1;
 
 public class ex109 {
     public static void main(String[] args) {
-
+        System.out.println(plus_r(3,6));
+        System.out.println(plus_it(3,6));
     }
+
+    static int inc(int x) { return x + 1; }
+
+    static int dec(int x) { return x - 1; }
+
+    static int plus_r(int a, int b) { return a == 0 ? b : inc(plus_r(dec(a), b)); }
+
+    static int plus_it(int a, int b) { return a == 0 ? b : plus_it(dec(a), inc(b)); }
 }
