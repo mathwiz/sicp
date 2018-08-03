a8405a85f040e9a4c3ba44268694382869c7cbdd
mathwiz <mathwiz@gmail.com>
2018-08-03 13:54:08 -0400

    iterative and recursive processes

--- ruby/ch1/ex109.rb
+++ ruby/ch1/ex109.rb
@@ -1,18 +1,10 @@
 module Ex109
-  def inc(x)
-    x + 1
-  end
-
-  def dec(x)
-    x - 1
-  end
-
   def plus_r(a, b)
-    a == 0 ? b : inc(plus_r(dec(a), b))
+    a == 0 ? b : inc(plus_r(a.pred, b))
   end
 
   def plus_it(a, b)
-    a == 0 ? b : plus_it(dec(a), inc(b))
+    a == 0 ? b : plus_it(a.pred, b.succ)
   end
 end
 
