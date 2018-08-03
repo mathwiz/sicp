04f82c9b72ae67f1a7606c9be68ef93b1f2f56d4
mathwiz <mathwiz@gmail.com>
2018-08-03 16:02:49 -0400

    recursive processes: Ackermann's function

--- haskell/ch1/Ex110.hs
+++ haskell/ch1/Ex110.hs
@@ -1,6 +1,10 @@
 module Ex110 where
 
-A _ 0 = 0
-A _ 1 = 2
-A 0 y = 2 * y
-A x y = A (pred x) (A x (pred y))
+a _ 0 = 0
+a _ 1 = 2
+a x y = if x==0 then 2 * y else a (pred x) (a x (pred y))
+
+f n = a 0 n
+g n = a 1 n
+h n = a 2 n
+k n = 5 * n * n
