7ba0633cb6347521de29f1c10ee8eeba7a7e6dd7
mathwiz <mathwiz@gmail.com>
2018-08-03 14:18:24 -0400

    iterative and recursive processes

--- scala/src/ch1/ex109.scala
+++ scala/src/ch1/ex109.scala
@@ -2,9 +2,15 @@ def inc(x: Int) = x + 1
 
 def dec(x: Int) = x - 1
 
-def plus_r(a: Int, b: Int): Int = if (a == 0) b else inc(plus_r(dec(a), b))
+def plus_r(a: Int, b: Int): Int = a match {
+  case 0 => b
+  case _ => inc(plus_r(dec(a), b))
+}
 
-def plus_it(a: Int, b: Int): Int = if (a == 0)  b else plus_it(dec(a), inc(b))
+def plus_it(a: Int, b: Int): Int = a match {
+  case 0 => b
+  case _ => plus_it(dec(a), inc(b))
+}
 
 def test_it(): String = {
   assert(inc(1) == 2)
