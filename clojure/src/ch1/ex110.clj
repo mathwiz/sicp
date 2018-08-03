da7b4f1c5361df24e0250d72bb536ba17bd58ef6
mathwiz <mathwiz@gmail.com>
2018-08-03 14:39:01 -0400

    recursive processes: Ackermann's function

--- clojure/src/ch1/ex110.clj
+++ clojure/src/ch1/ex110.clj
@@ -1,2 +1,13 @@
 (defn A [x y]
-  (cond ))
+  (cond (= y 0) 0
+        (= x 0) (* 2 y)
+        (= y 1) 2
+        :else (A (- x 1)
+                 (A x (- y 1)))))
+
+(require
+ 'clojure.test)
+
+(clojure.test/is (= (A 1 10) 1024))
+(clojure.test/is (= (A 2 4) 65536))
+(clojure.test/is (= (A 3 3) 65536))
