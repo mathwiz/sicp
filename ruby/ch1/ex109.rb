933c11a3e377c7ef9cc2a14040ed02715d7d7b14
mathwiz <mathwiz@gmail.com>
2018-08-03 09:21:44 -0400

    iterative and recursive processes

--- ruby/ch1/ex109.rb
+++ ruby/ch1/ex109.rb
@@ -15,3 +15,23 @@ module Ex109
     a == 0 ? b : plus_it(dec(a), inc(b))
   end
 end
+
+require 'test/unit'
+class Ex109Test < Test::Unit::TestCase
+  include Ex109
+
+  def test_plus_r
+    assert_equal(3, plus_r(1,2))
+    assert_equal(3, plus_r(2,1))
+    assert_equal(2, plus_r(0,2))
+    assert_equal(2, plus_r(2,0))
+  end
+
+  def test_plus_it
+    assert_equal(3, plus_it(1,2))
+    assert_equal(3, plus_it(2,1))
+    assert_equal(2, plus_it(0,2))
+    assert_equal(2, plus_it(2,0))
+  end
+
+end
