b1f1568cb0cbc1b28f84e2a654660695e3d99b2c
mathwiz <mathwiz@gmail.com>
2018-08-03 14:48:58 -0400

    recursive processes: Ackermann's function

--- clojure/src/ch1/ex110.clj
+++ clojure/src/ch1/ex110.clj
@@ -5,9 +5,20 @@
         :else (A (- x 1)
                  (A x (- y 1)))))
 
+(defn f [n] (A 0 n)) ;; 2n
+(defn g [n] (A 1 n)) ;; 2^n
+(defn h [n] (A 2 n)) ;; ???
+(defn k [n] (* 5 n n))  ;; 5n^2
+
+
 (require
  'clojure.test)
 
 (clojure.test/is (= (A 1 10) 1024))
 (clojure.test/is (= (A 2 4) 65536))
 (clojure.test/is (= (A 3 3) 65536))
+(clojure.test/is (= (f 4) 8))
+(clojure.test/is (= (f 78) 156))
+(clojure.test/is (= (g 4) 16))
+(clojure.test/is (= (h 4) 65536))
+
