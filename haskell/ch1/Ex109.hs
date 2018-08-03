f1c36842414fc8a2b840e0d32e23ca0d92934370
mathwiz <mathwiz@gmail.com>
2018-08-03 14:05:35 -0400

    iterative and recursive processes

--- haskell/ch1/Ex109.hs
+++ haskell/ch1/Ex109.hs
@@ -1,14 +1,10 @@
 module Ex109 where
 
-inc x = x + 1
-
-dec x = x - 1
-
 plus_r a b =
   if a == 0 then b
-  else inc (plus_r (dec a) b)
+  else succ (plus_r (pred a) b)
                         
 plus_it a b =
   if a == 0 then b
-  else plus_it (dec a) (inc b)
+  else plus_it (pred a) (succ b)
                         
