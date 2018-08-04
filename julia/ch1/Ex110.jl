b199384c038375508666a73be5976125f8f395cf
mathwiz <mathwiz@gmail.com>
2018-08-04 11:32:07 -0400

    commit test

--- julia/ch1/Ex110.jl
+++ julia/ch1/Ex110.jl
@@ -7,4 +7,24 @@ Ex110:
 
 module Ex110
 
+function a(x, y)
+  if y == 0
+    return 0
+  elseif x == 0
+    return 2 * y
+  elseif y == 1
+    return 2
+  else
+    a(x - 1, a(x, y - 1))
+  end
+end
+
+f(n) = a(0, n)
+
+g(n) = a(1, n)
+
+h(n) = a(2, n)
+
+k(n) = 5 * n * n
+
 end
