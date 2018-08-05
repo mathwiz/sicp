9d6bc6ffb665c90edc30322f50970a604f1d79f1
mathwiz <mathwiz@gmail.com>
2018-08-04 20:34:00 -0400

    formatting

--- julia/ch1/Ex110.jl
+++ julia/ch1/Ex110.jl
@@ -8,15 +8,16 @@ Ex110:
 module Ex110
 
 function a(x, y)
-  if y == 0
-    return 0
-  elseif x == 0
-    return 2 * y
-  elseif y == 1
-    return 2
-  else
-    a(x - 1, a(x, y - 1))
-  end
+    if y == 0
+        return 0
+    elseif x == 0
+        return 2 * y
+    elseif y == 1
+        return 2
+    else
+        a(x - 1, a(x, y - 1))
+    end
+
 end
 
 f(n) = a(0, n)
