2f3cf46f8d05f501e348e5281516c120e362a21b
mathwiz <mathwiz@gmail.com>
2018-08-04 20:25:55 -0400

    adding octave

---
+++ octave/ch1/Ex101.m
@@ -0,0 +1,27 @@
+# load in gui
+
+10
+
+5 + 3 + 4
+
+9 - 1
+
+6 / 2
+
+(2 * 4) + (4 - 6)
+
+a = 3
+
+b = a + 1
+
+a + b + (a * b)
+
+a == b
+
+ifelse((b > a) && (b < a*b), b, a)
+
+ifelse((a==4), 6, ifelse(b==4, 6+7+a, 25))
+
+2 + ifelse(b > a, b, a)
+
+ifelse(a > b, a, ifelse(a < b, b, -1)) * (a + 1)
