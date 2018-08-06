e470841544cfc409cbb3c3d5642b2ba4d3719a6e
mathwiz <mathwiz@gmail.com>
2018-08-05 21:39:15 -0400

    ex 105

--- octave/ch1/Ex105.m
+++ octave/ch1/Ex105.m
@@ -2,6 +2,3 @@ function retval = Ex105(x, y)
   retval = ifelse(x == 0, 0, y) 
 endfunction
 
-function retval = p
-  retval = p
-endfunction
---
+++ octave/ch1/Ex105_p.m
@@ -0,0 +1,8 @@
+# Causes the following
+# error: max_recursion_depth exceeded
+# error: called from
+#    Ex105_p at line 2 column 10
+
+function retval = Ex105_p
+  retval = Ex105_p
+endfunction
