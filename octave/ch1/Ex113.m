915b8522ae0bd18ad4a0fb5f201440acf970016b
mathwiz <mathwiz@gmail.com>
2019-02-03 18:51:21 -0500

    Ex113

---
+++ octave/ch1/Ex113.m
@@ -0,0 +1,27 @@
+function Ex113
+  fib(0)
+  fib(1)
+  fib(2)
+  fib(3)
+  fib(10)
+  fib(15)
+endfunction
+
+function retval = fib(n)
+  if n < 2
+    retval = n;
+    return;
+  else
+    retval = round((psi(n) - phi(n)) / sqrt(5));
+  endif
+endfunction
+
+function retval = psi(n)
+  retval = ((1 + sqrt(5)) / 2) ^ n;
+  return; 
+endfunction
+
+function retval = phi(n)
+  retval = ((1 - sqrt(5)) / 2) ^ n;
+  return; 
+endfunction
