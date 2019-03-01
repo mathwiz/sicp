ba96d59e2950801ea9a219a2673f391ca4c7ab5c
mathwiz <mathwiz@gmail.com>
2019-03-01 14:47:27 -0500

    Ex115

---
+++ racket/ch1/ex115.rkt
@@ -0,0 +1,24 @@
+#lang racket
+
+
+;;; Model sine function via:
+;;; sin(x) approx= x if x is small, and the identity
+;;; sin(x) = 3sin(x/3) - 4(sin(x/3))^3
+
+(define (cube x) (* x x x))
+
+(define (p x)
+  (begin
+    (trace x)
+    (- (* 3 x) (* 4 (cube x)))))
+
+(define (sine angle)
+  (if (not (> (abs angle) 0.1))
+      angle
+      (p (sine (/ angle 3.0)))))
+
+(define (trace arg)
+  (display "got here with arg ") (display arg) (newline))
+
+;;; a) How many times is p applied when (sine 12.15) is evaluated? 5
+;;; b) What is the order of growth in space and number of steps (as a function of a) when (sine a) is evaluated? O(log a)
