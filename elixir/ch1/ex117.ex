bc8baafd1cb671b043c973ff577e3b4c547212fd
mathwiz <mathwiz@gmail.com>
2019-03-27 14:20:00 -0400

    Ex117

---
+++ elixir/ch1/ex117.ex
@@ -0,0 +1,16 @@
+# cd("c:/Users/Yohan/IdeaProjects/sicp/elixir/ch1")
+# Load with import_file("filename")
+# Run with Module.func(args)
+defmodule Ex117 do
+  @moduledoc false
+  def square(x), do: x * x
+
+  def expt_invariant(_, 0, a), do: a
+  def expt_invariant(b, p, a) when rem(p,2) == 0, do: expt_invariant(square(b), div(p,2), a)
+  def expt_invariant(b, p, a), do: expt_invariant(b, p-1, a*b)
+
+  def expt(b, p), do: expt_invariant(b, p, 1)
+
+  def times(a, 0), do: 0
+  def times(a, b), do: a + times(a, b-1)
+end
