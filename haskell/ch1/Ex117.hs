f34e46d41021895791e53670589c6695739e23d5
mathwiz <mathwiz@gmail.com>
2019-10-29 14:06:21 -0400

    Ex117

--- haskell/ch1/Ex117.hs
+++ haskell/ch1/Ex117.hs
@@ -4,11 +4,8 @@
 
 module Ex117 where
 
-expt_invariant _ 0 a = a
-expt_invariant b p a | even p = expt_invariant (b*b) (div p 2) a
-expt_invariant b p a = expt_invariant b (p - 1) (a * b)
+mult_invariant _ 0 a = a
+mult_invariant b p a | even p = mult_invariant (b*2) (div p 2) a
+mult_invariant b p a = mult_invariant b (p - 1) (a + b)
 
-expt b p = expt_invariant b p 1
-
-times _ 0 = 0
-times a b = a + times a (b-1)
+times a b = mult_invariant a b 0
