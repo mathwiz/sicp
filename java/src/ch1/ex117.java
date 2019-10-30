8ed3692a4624cdf785bfd597191c738e153e6c61
mathwiz <mathwiz@gmail.com>
2019-10-30 14:14:35 -0400

    Ex117

--- java/src/ch1/ex117.java
+++ java/src/ch1/ex117.java
@@ -1,41 +1,25 @@
 package ch1;
 
-import java.util.function.BiFunction;
-
 public class ex117 {
     public static void main(String[] args) {
-        System.out.println(expt(2, 0));
-        System.out.println(expt(2, 4));
-        System.out.println(expt(2, 9));
-        System.out.println(expt(3, 4));
-        System.out.println(expt(3, 5));
-        BiFunction<Integer, Integer, Integer> times = makeMult();
-        System.out.println(times .apply(9, 5));
-        System.out.println(times .apply(13, 12));
+        Times func = (int i, int i2) -> mult_invariant(i, i2, 0);
+
+        System.out.println(func.apply(9, 5));
+        System.out.println(func.apply(9, 8));
+        System.out.println(func.apply(9, 12));
+        System.out.println(func.apply(9, 100));
         System.out.println("done");
     }
 
-    static BiFunction<Integer, Integer, Integer> makeMult() {
-        return new BiFunction<Integer, Integer, Integer>() {
-            @Override
-            public Integer apply(Integer integer, Integer integer2) {
-                return multiply(integer, integer2);
-            }
-
-            private int multiply(int a, int b) {
-                return b == 0 ? 0 : a + multiply(a, b - 1);
-            }
-        };
+    @FunctionalInterface
+    interface Times {
+        int apply(int i, int i2);
     }
 
-    static int expt(int b, int p) {
-        return expt_invariant(b, p, 1);
-    }
-
-    static int expt_invariant(int b, int p, int a) {
+    static int mult_invariant(int b, int p, int a) {
         if (p == 0) return a;
-        if (even(p)) return expt_invariant(b * b, p / 2, a);
-        return expt_invariant(b, p - 1, a * b);
+        if (even(p)) return mult_invariant(b * 2, p / 2, a);
+        return mult_invariant(b, p - 1, a + b);
     }
 
     static boolean even(int x) {
