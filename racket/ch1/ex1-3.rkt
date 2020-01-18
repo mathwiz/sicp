742c60d64134a1b3b25440ed5705046a55a57ece
Yohan Lee <yohanlee@yahoo.com>
2020-01-18 16:07:12 -0500

    racket

--- racket/ch1/ex1-3.rkt
+++ racket/ch1/ex1-3.rkt
@@ -1,6 +1,18 @@
-#lang racket/load
-(require rackunit)
-(require "../functions.rkt")
+;; run with racket -f ex1-3.rkt
+
+
+(define (squares xs)
+  (cond ((null? xs) xs)
+        (else 
+         (let ((hd (first xs)))
+           (cons (* hd hd) (squares (rest xs)))))))
+
+(define (sum-squares xs)
+  (letrec ((sqs (squares xs))
+           (recur (lambda (ss acc)
+                    (cond ((null? ss) acc)
+                          (else (recur (rest ss) (+ (first ss) acc)))))))
+    (recur sqs 0)))
 
 (define (larger-sq x y z)
   (cond ((and 
@@ -17,12 +29,12 @@
          (sum-squares (list x y)))))
 
 
-(check-eq? (larger-sq 2 3 4) 25)
-(check-eq? (larger-sq 2 4 3) 25)
-(check-eq? (larger-sq 3 2 4) 25)
-(check-eq? (larger-sq 3 4 2) 25)
-(check-eq? (larger-sq 4 3 2) 25)
-(check-eq? (larger-sq 4 2 3) 25)
-(check-eq? (larger-sq 5 3 4) 41)
-(check-eq? (larger-sq 2 2 2) 8)
-(write 'done)
+(println (= (larger-sq 2 3 4) 25))
+(println (= (larger-sq 2 4 3) 25))
+(println (= (larger-sq 3 2 4) 25))
+(println (= (larger-sq 3 4 2) 25))
+(println (= (larger-sq 4 3 2) 25))
+(println (= (larger-sq 4 2 3) 25))
+(println (= (larger-sq 5 3 4) 41))
+(println (= (larger-sq 2 2 2) 8))
+(writeln 'done)
