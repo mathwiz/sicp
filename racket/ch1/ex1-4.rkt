e81761bd458bc71fc37ae738be02351c19839d22
Yohan Lee <yohanlee@yahoo.com>
2020-01-18 16:10:31 -0500

    racket

--- racket/ch1/ex1-4.rkt
+++ racket/ch1/ex1-4.rkt
@@ -1,11 +1,8 @@
-#lang racket
-(require rackunit)
-
 (define (a-plus-abs-b a b)
   ((if (> b 0) + -) a b))
 
-(check-eq? (a-plus-abs-b 1 2) 3)
-(check-eq? (a-plus-abs-b 1 -2) 3)
-(check-eq? (a-plus-abs-b -1 2) 1)
-(check-eq? (a-plus-abs-b -1 -2) 1)
-(write 'done)
+(println (= (a-plus-abs-b 1 2) 3))
+(println (= (a-plus-abs-b 1 -2) 3))
+(println (= (a-plus-abs-b -1 2) 1))
+(println (= (a-plus-abs-b -1 -2) 1))
+(writeln 'done)
