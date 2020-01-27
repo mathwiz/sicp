901e8b9909f6eeb3c6369b7897a192c13ce81d47
Yohan Lee <yohanlee@yahoo.com>
2020-01-27 15:42:45 -0500

    racket ex 10

--- racket/ch1/ex110.rkt
+++ racket/ch1/ex110.rkt
@@ -1,6 +1,3 @@
-#lang racket
-(require rackunit)
-
 (define (A x y)
   (cond ((= y 0) 0)
         ((= x 0) (* 2 y))
@@ -8,21 +5,25 @@
         (else (A (- x 1)
                (A x (- y 1))))))
 
+
 (define (f n) (A 0 n)) ;; 2n
 (define (g n) (A 1 n)) ;; 2^n
 (define (h n) (A 2 n)) ;; ???
 (define (k n) (* 5 n n))  ;; 5n^2
 
-(check-true (= (A 0 0) 0))
-(check-true (= (A 1 0) 0))
-(check-true (= (A 0 1) 2))
-(check-true (= (A 1 1) 2))
-(check-true (= (A 0 2) 4))
-(check-true (= (A 1 10) 1024))
-(check-true (= (A 2 4) 65536))
-(check-true (= (A 3 3) 65536))
-(check-true (= (f 4) 8))
-(check-true (= (f 78) 156))
-(check-true (= (g 4) 16))
-(check-true (= (h 4) 65536))
-(write 'done)
+(println "begin testing")
+
+(println (= (A 0 0) 0))
+(println (= (A 1 0) 0))
+(println (= (A 0 1) 2))
+(println (= (A 1 1) 2))
+(println (= (A 0 2) 4))
+(println (= (A 1 10) 1024))
+(println (= (A 2 4) 65536))
+(println (= (A 3 3) 65536))
+(println (= (f 4) 8))
+(println (= (f 78) 156))
+(println (= (g 4) 16))
+(println (= (h 4) 65536))
+
+(writeln 'done)
