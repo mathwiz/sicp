aa1c1e2b74539f382972962d1058caabd36e26c3
Yohan Lee <yohanlee@yahoo.com>
2020-01-31 11:17:54 -0500

    lisp ex 18

---
+++ lisp/ch1/Ex118.lisp
@@ -0,0 +1,31 @@
+;; Load with (load "file")
+;; or run > clisp file
+
+(defun fib (n)
+  (fib-iter 1 0 0 1 n))
+
+(defun fib-iter (a b p q count)
+  (cond ((= count 0) b)
+        ((evenp count)
+         (fib-iter a
+                   b
+                   NIL
+                   NIL
+                   (/ count 2)))
+        (t (fib-iter (+ (* b q) (* a q) (* a p))
+                     (+ (* b p) (* a q))
+                     p
+                     q
+                     (- count 1)))))
+
+
+(print (= (fib 0) 0))
+(print (= (fib 1) 1))
+(print (= (fib 2) 2))
+(print (= (fib 3) 3))
+(print (= (fib 4) 5))
+(print (= (fib 5) 8))
+(print (= (fib 6) 13))
+(print (fib 6))
+
+(print "done")
