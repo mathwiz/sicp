d515392f99573bfaf2a92fe8e203018719b2321d
Yohan Lee <yohanlee@yahoo.com>
2020-03-17 20:28:02 -0400

    ex 120

---
+++ lisp/ch1/Ex120.lisp
@@ -0,0 +1,8 @@
+(defun verbose-gcd (a b)
+  (cond ((= b 0) (progn
+                   (format t "(verbose-gcd ~D ~D)~%" a b)
+                   a))
+        (t (progn
+             (format t "(mod ~D ~D)~%" a b)
+             (verbose-gcd b (mod a b))))))
+
