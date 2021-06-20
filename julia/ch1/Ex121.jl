793980ea736e4f965665aa7644f8be0e47ca3958
mathwiz <mathwiz@gmail.com>
2021-06-20 10:35:44 -0400

    ex 122

--- julia/ch1/Ex121.jl
+++ julia/ch1/Ex121.jl
@@ -11,5 +11,16 @@ sq(n) = n * n
 
 divides(a, b) = b % a == 0
 
+function find_divisor(n, test)
+    if sq(test) > n
+        return n
+    elseif divides(test, n)
+        return test
+    else
+        return find_divisor(n, test + 1)
+    end
+end
+
+smallest_divisor(n) = find_divisor(n, 2)
 
 end
