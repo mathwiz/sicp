64751dd3a7729b0e1bbb86c080da3b25247a661f
Yohan Lee <yohanlee@yahoo.com>
2021-07-01 12:35:21 -0400

    Ex 125

--- racket/ch1/ex125.rkt
+++ racket/ch1/ex125.rkt
@@ -3,13 +3,13 @@
 (provide search-for-primes)
 (provide search-for-primes-fast)
 
+(define (fast-expt b n)
+  (cond ((= n 0) 1)
+        ((even? n) (square (fast-expt b (/ n 2))))
+        (else (* b (fast-expt b (- n 1))))))
 
 (define (expmod base exp m)
-  (cond ((= exp 0) 1)
-        ((even? exp)
-         (remainder (square (expmod base (/ exp 2) m)) m))
-        (else
-         (remainder (* base (expmod base (- exp 1) m)) m))))
+  (remainder (fast-expt base exp) m))
 
 (define (fermat-test n)
   (define (try-it a)
