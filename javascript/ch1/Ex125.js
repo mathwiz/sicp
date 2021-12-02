c0cffa9971d3ce31179d4e898d8b1d61463ee1b3
Yohan Lee <yohanlee@yahoo.com>
2021-12-02 16:57:30 -0500

    Ex125

---
+++ javascript/ch1/Ex125.js
@@ -0,0 +1,85 @@
+// Load with: const ExXXX = require('./ExXXX')
+// Run with: ExXXX.xxx()
+
+let fast_times = 3;
+
+let sq = n => n * n;
+
+let divides = (a, b) => (b % a) == 0;
+
+let find_divisor = (n, test_divisor) => {
+    if (sq(test_divisor) > n) {
+        return n;
+    } else if (divides(test_divisor, n)) {
+        return test_divisor;
+    } else {
+        return find_divisor(n, test_divisor + 1);
+    }
+};
+
+let smallest_divisor = n => find_divisor(n, 2);
+
+let odd = n => !divides(2, n);
+
+//let is_prime = n => n == smallest_divisor(n);
+let is_prime = n => fast_prime(n, fast_times);
+
+let fast_prime = (n, times) => {
+    if (times==0) {
+        return true;
+    } else if (fermat_test(n)) {
+        return fast_prime(n, times-1);
+    } else {
+        return false;
+    }
+};
+
+let fermat_test = n => {
+    let try_it = a => a == expmod(a, n, n);
+    return try_it(1 + random(n-1));
+}
+
+// Breaks search_for_primes. No idea why.
+let expmod = (base, exp, m) => {
+    return fast_expt(base, exp) % m;
+};
+
+let fast_expt = (base, exp) => {
+    if (exp==0) {
+        return 1;
+    } else if (!odd(exp)) {
+        return sq(fast_expt(base, exp/2));
+    } else {
+        return (base * fast_expt(base, exp-1));
+    }
+};
+
+let random = limit => Math.floor(limit * Math.random());
+
+let prime_test = n => {
+    if (is_prime(n)) {
+        console.log(n + " ***");
+    } else {
+        console.log(n);
+    }
+};
+
+let search_helper = (n, end) => {
+    if (n <= end) {
+        prime_test(n);
+        search_helper(n + 2, end);
+    } else {
+        console.log("finished\n");
+    }
+};
+
+let search_for_primes = (start, end) => search_helper(odd(start) ? start : start + 1, end);
+
+exports.search_for_primes = search_for_primes;
+exports.random = random;
+
+exports.test_it = function () {
+    search_for_primes(1000, 1020);
+    search_for_primes(10000, 10040);
+    search_for_primes(100000, 100080);
+};
