1771fb1f5f0359546fd278a4cdb63a60748bc055
Yohan Lee <yohanlee@yahoo.com>
2021-12-03 09:17:43 -0500

    Ex123

---
+++ octave/ch1/Ex123.m
@@ -0,0 +1,31 @@
+function Ex123
+  printf("%d\n", smallest_divisor(199));
+  printf("%d\n", smallest_divisor(1999));
+  printf("%d\n", smallest_divisor(19999));
+  printf("done\n");
+endfunction
+
+
+function VAL = find_divisor(n, test)
+  divides = @(a, b) rem(b, a) == 0;
+  
+  if (test * test > n)
+    VAL = n;
+  elseif (divides(test, n))
+    VAL = test;
+  else
+    VAL = find_divisor(n, test + 1);
+  endif
+endfunction
+
+function VAL = next(n)
+  if n == 2
+    VAL = 3;
+  else
+    VAL = n + 2;
+  endif
+endfunction
+
+function VAL = smallest_divisor(n) 
+  VAL = find_divisor(n, 2);
+endfunction
