cd923bc6ca134884401d96d70fa570eb0823b57c
Yohan Lee <yohanlee@yahoo.com>
2021-12-05 13:41:37 -0500

    Ex125

---
+++ scala/src/ch1/ex126.scala
@@ -0,0 +1,69 @@
+// run with> scala exXXX.scala
+
+import java.util.Random
+
+def divides(a: BigInt, b: BigInt): Boolean = b % a == 0
+
+def smallest_divisor(n: Int): Int = find_divisor(n, 2)
+
+def find_divisor(n: Int, test: Int): Int = test match {
+  case x if x * x > n => n
+  case x if divides(test, n) => test
+  case _ => find_divisor(n, test + 1)
+}
+
+//def prime(n: Int) = n == smallest_divisor(n)
+def prime(n: Int) = fast_prime(n, 3)
+
+def fast_prime(n: Int, times: Int): Boolean = times match {
+  case 0 => true
+  case _ if fermat_test(n) => fast_prime(n, times-1)
+  case _ => false
+}
+
+def fermat_test(n: Int) : Boolean = {
+  val try_it = (a: Int) => a == expmod(a, n, n)
+  try_it(1 + random(n-1))
+}
+
+// Inefficiently use 2 function calls instead of calling square
+def expmod(base: BigInt, exp: Int, m: BigInt): BigInt = exp match {
+  case 0 => BigInt(1)
+  case _ if divides(2, exp) => (expmod(base, exp/2, m) * expmod(base, exp/2, m) % m
+  case _ => ((base * expmod(base, exp-1, m)) % m)
+}
+
+def square(n: BigInt): BigInt = n * n
+
+def random(limit: Int): Int = {
+  new Random().nextInt(limit)
+}
+
+def report_prime(n: Int) = {
+  if (prime(n)) {
+    println(n + " ***")
+  } else {
+    println(n)
+  }
+}
+
+def search_helper(n: Int, finish: Int): Int = {
+  if (n <= finish) {
+    report_prime(n)
+    search_helper(n + 2, finish)
+  } else {
+    println("finished\n")
+    0
+  }
+}
+
+def search_for_primes(start: Int, finish: Int) =
+  search_helper(if (divides(2, start)) start + 1 else start, finish)
+
+
+// testing
+search_for_primes(1000, 1020)
+search_for_primes(10000, 10040)
+search_for_primes(100000, 100080)
+
+println("done")
