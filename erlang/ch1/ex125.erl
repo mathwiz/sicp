22e5a5e3dce7ca26e5183f6244944f0d82470515
Yohan Lee <yohanlee@yahoo.com>
2021-12-05 17:09:56 -0500

    Ex125

--- erlang/ch1/ex125.erl
+++ erlang/ch1/ex125.erl
@@ -46,11 +46,13 @@ random(Limit) -> rand:uniform(Limit) - 1.
 
 even(N) -> N rem 2 == 0.
 
-fast_expt(_, 0, _) -> 1;
 fast_expt(Base, Exp) ->
-  case even(Exp) of
-    true -> square(fast_expt(Base, Exp div 2));
-    false -> Base * fast_expt(Base, Exp - 1)
+  if 
+    Exp == 0 -> 1;
+    true -> 
+      case even(Exp) of
+        true -> square(fast_expt(Base, Exp div 2));
+        false -> Base * fast_expt(Base, Exp - 1)
   end.
 
 %% Does this break?
