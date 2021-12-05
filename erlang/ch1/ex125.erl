0227f559045fd79ab2a222fb3246d3349c84cf69
Yohan Lee <yohanlee@yahoo.com>
2021-12-05 17:18:56 -0500

    Ex125

--- erlang/ch1/ex125.erl
+++ erlang/ch1/ex125.erl
@@ -46,10 +46,10 @@ random(Limit) -> rand:uniform(Limit) - 1.
 
 even(N) -> N rem 2 == 0.
 
+fast_expt(_, 0) -> 0;
 fast_expt(Base, Exp) ->
-  case Exp of
-    0 -> 1;
-    even(Exp) -> square(fast_expt(Base, Exp div 2));
+  case even(Exp) of
+    true -> square(fast_expt(Base, Exp div 2));
     false -> Base * fast_expt(Base, Exp - 1)
   end.
 
