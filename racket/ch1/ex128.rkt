50059db5d1b8272beddd1ce325b0921b968ab635
Yohan Lee <yohanlee@yahoo.com>
2021-12-06 19:00:36 -0500

    Ex128

--- racket/ch1/ex128.rkt
+++ racket/ch1/ex128.rkt
@@ -31,11 +31,21 @@
 (define (miller-rabin-test n)
   (define (try-it a)
     (let ((result (miller-rabin-expmod a (sub1 n) n)))
-      (or (= result 0) (= result 1))))
+      (= result 1)))
       (try-it (+ 1 (random (- n 1)))))
 
 (define (prime? n)
   (= n (smallest-divisor n)))
+
+(define (fast-prime? n times)
+  (cond ((= times 0) true)
+        ((fermat-test n) (fast-prime? n (- times 1)))
+        (else false)))
+
+(define (fast-miller-rabin? n times)
+  (cond ((= times 0) true)
+        ((miller-rabin-test n) (fast-miller-rabin? n (- times 1)))
+        (else false)))
 
 (define (smallest-divisor n)
   (find-divisor n 2))
@@ -57,12 +67,20 @@
 (define (my-test n)
   (writeln n)
   (writeln (prime? n))
-  (writeln (fermat-test n))
-  (writeln (miller-rabin-test n))
+  (writeln (fast-prime? n 100))
+  (writeln (fast-miller-rabin? n 100))
   (writeln 'done))
 
-(my-test 10)
+;; Primes
 (my-test 11)
+(my-test 13)
+(my-test 97)
+(my-test 127)
+;; Non-primes
+(my-test 9)
+(my-test 77)
+(my-test 6603)
+(my-test 16601)
 ;; Carmichael numbers 561, 1105, 1729, 2465, 2821, 6601
 (my-test 561)
 (my-test 1105)
@@ -70,7 +88,5 @@
 (my-test 2465)
 (my-test 2821)
 (my-test 6601)
-;; A non-prime
-(my-test 6603)
 
 
