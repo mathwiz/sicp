69da5336f90eaccd476801ac3feafe47ab268840
Yohan Lee <yohanlee@yahoo.com>
2022-01-11 16:35:15 -0500

    Ex128

--- java/src/ch1/Ex128.java
+++ java/src/ch1/Ex128.java
@@ -71,7 +71,7 @@ public class Ex128 {
     }    
 
     static Function<Integer, Boolean> miller_rabin_test = new Function<Integer, Boolean>() {
-        Boolean apply(Integer n) {
+        public Boolean apply(Integer n) {
             Function<Integer, Boolean> f = a -> 1 == new MillerRabinExpmod(a, n-1, n).value();
             return f.apply(1 + random.apply(n-1));
         }
