3d44c4423995149a4016fb7eb85445dfd2d48989
Yohan Lee <yohanlee@yahoo.com>
2022-01-13 11:33:50 -0500

    Ex127

---
+++ erlang/ch1/ex127.erl
@@ -0,0 +1,72 @@
+%%%-------------------------------------------------------------------
+%%% @author Yohan Lee
+%%% @copyright (C) 2019
+%%% @doc
+%%% cd("c:/Users/Yohan/IdeaProjects/sicp/erlang/ch1").
+%%% compile with > c("exXXX.erl").
+%%% if already compiled, load with > l(exXXX).
+%%% run with > exXXX:main().
+%%%
+%%% @end
+%%%-------------------------------------------------------------------
+-module(ex124).
+-author("Yohan").
+
+%% API
+-export([main/0]).
+
+main() ->
+  io:fwrite("~w\n", [test_number(10)]),
+  io:fwrite("~w\n", [test_number(11)]),
+  io:fwrite("~w\n", [test_number(561)]),
+  io:fwrite("~w\n", [test_number(1105)]),
+  io:fwrite("~w\n", [test_number(1729)]),
+  io:fwrite("~w\n", [test_number(2465)]),
+  io:fwrite("~w\n", [test_number(2821)]),
+  io:fwrite("~w\n", [test_number(6601)]),
+  io:fwrite("~w\n", [test_number(6603)]).
+
+
+test_number(N) ->
+  io_lib:format("~w: \tPrime:~w \tFast Prime:~w", [N, prime(N), fast_prime(N)]).
+
+square(N) -> N * N.
+
+random(Limit) -> rand:uniform(Limit) - 1.
+
+even(N) -> N rem 2 == 0.
+
+smallest_divisor(N) -> find_divisor(N, 2).
+
+find_divisor(N, Test) when Test * Test > N -> N;
+find_divisor(N, Test) ->
+  if
+    N rem Test == 0 -> Test;
+    true -> find_divisor(N, Test + 1)
+  end.
+
+prime(N) -> N == smallest_divisor(N).
+
+fast_prime(_, 0) -> true;
+fast_prime(N, Times) ->
+  case fermat_test(N) of
+    true -> fast_prime(N, Times-1);
+    false -> false
+  end.
+
+fermat_test(N) ->
+  TryIt = fun (A) -> expmod(A, N, N) == A end,
+  TryIt(1 + random(N - 1)).
+
+expmod(_, 0, _) -> 1;
+expmod(Base, Exp, M) ->
+  case even(Exp) of
+    true -> square(expmod(Base, Exp div 2, M)) rem M;
+    false -> Base * expmod(Base, Exp - 1, M) rem M
+  end.
+
+
+
+
+
+
